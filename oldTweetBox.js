16c1780a868644c26b41aa02be3d3d5b19ded57b
3846masa <3846masahiro+git@gmail.com>
2014-11-06 17:13:30 +0900

    first commit

---
+++ oldTweetBox.js
@@ -0,0 +1,3 @@
+$('.timeline-tweet-box').appendTo($('.DashboardProfileCard-content'));
+$('.top-timeline-tweet-box-user-image').hide();
+$('.stream-container').css({"border-radius": "4px 4px 0 0", "overflow": "hidden"});
