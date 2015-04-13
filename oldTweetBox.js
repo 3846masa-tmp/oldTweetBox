4ca426a425e5143828593683db50ddb6c28ab38c
3846masa <3846masahiro+git@gmail.com>
2015-04-13 15:26:59 +0900

    ちょっと直した

--- oldTweetBox.js
+++ oldTweetBox.js
@@ -1,3 +1,8 @@
-$('.timeline-tweet-box').appendTo($('.DashboardProfileCard-content'));
-$('.top-timeline-tweet-box-user-image').hide();
-$('.stream-container').css({"border-radius": "4px 4px 0 0", "overflow": "hidden"});
+setTimeout(function fixToPosition() {
+  if ($('#timeline > .timeline-tweet-box').length === 1) {
+    $('.timeline-tweet-box').appendTo($('.DashboardProfileCard-content'));
+    $('.top-timeline-tweet-box-user-image').hide();
+    $('.stream-container').css({"border-radius": "4px 4px 0 0", "overflow": "hidden"});
+  }
+  setTimeout(fixToPosition, 100);
+}, 100);
